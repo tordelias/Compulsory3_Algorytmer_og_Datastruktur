b28f728cd8f35047b6a1b5fa1be0805e7072584e
tordelias <tordeliashoumb46@gmail.com>
2023-10-24 11:59:45 +0200

    added all the functions that need to be made

--- Compulsory3_Trees_and_Graphs/Compulsory3_Trees_and_Graphs/Graph.h
+++ Compulsory3_Trees_and_Graphs/Compulsory3_Trees_and_Graphs/Graph.h
@@ -4,14 +4,54 @@
 using namespace std; 
 class Graph
 {
+public: 
 	struct Node
 	{
-		int data;
+		int data, cost;
 		int numvertecies; 
 		list<Node*> adjlist;
 
 	};
-	Node* CreateGraph()
+	Node* CreateGraph(int data, int weight, Node* head)
+	{
+		Node* newNode = new Node;
+		newNode->data = data; 
+		newNode->cost = weight; 
+		
+		if(head != nullptr)
+		newNode->adjlist.push_back(head); 
+
+		return newNode; 
+	}
+	void returnNodeinf()
+	{
+
+	}
+	void adjNodes()
+	{
+
+	}
+	void verticesGraph()
+	{
+
+	}
+	void edgeGraph()
+	{
+
+	}
+	int size()
+	{
+
+	}
+	bool isEmpty()
+	{
+
+	}
+	void deleteNode()
+	{
+
+	}
+	void deleteEdge()
 	{
 
 	}
